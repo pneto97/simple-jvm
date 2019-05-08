a061808c8ee9b43c7005d27ba44e33ae31ce1f28
Paulo Neto <paulo_btn@yahoo.com.br>
2019-05-08 11:31:15 -0300

    verificacao se o usuario especificou o arquivo .class

--- class_displayer/main.c
+++ class_displayer/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "j8_class_structure.h"
 
 
-int main(){
+int main(int argc , char* argv[]){
+
+    if(argc != 2){
+        printf("Especifique um arquivo .class!\n");
+        exit(1);
+    }
+
     class_structure* java_class;
     java_class = ( class_structure* ) malloc( sizeof(class_structure) );
 
     printf("Rodou!\n");
 
     free(java_class);
+
+    return 0;
 }
