534f2096bc9940c52b09e3cb1c3d5fce1a07eca5
Paulo Neto <paulo_btn@yahoo.com.br>
2019-05-09 09:52:13 -0300

    criei funcoes pra ler, inverter bytes e fiz o teste do magic number

--- class_displayer/main.c
+++ class_displayer/main.c
@@ -3,6 +3,38 @@
 #include <string.h>
 #include "j8_class_structure.h"
 
+uint16_t reverse16(uint16_t a){
+    a = ((a & 0x00FF) << 8) | ((a & 0xFF00) >> 8);
+    return a;
+}
+
+uint32_t reverse32(uint32_t a){
+  a = ((a & 0x000000FF) << 24) |
+    ((a & 0x0000FF00) <<  8) |
+    ((a & 0x00FF0000) >>  8) |
+    ((a & 0xFF000000) >> 24);
+    return a;
+}
+
+uint8_t read8(FILE* fp){
+    uint8_t buff = 0; 
+    fread(&buff,sizeof(buff),1,fp);
+    return buff;
+}
+
+uint16_t read16(FILE* fp){
+    uint16_t buff = 0; 
+    fread(&buff,sizeof(buff),1,fp);
+    buff = reverse16(buff);
+    return buff;
+}
+
+uint32_t read32(FILE* fp){
+    uint32_t buff = 0; 
+    fread(&buff,sizeof(buff),1,fp);
+    buff = reverse32(buff);
+    return buff;
+}
 
 int main(int argc , char* argv[]){
 
@@ -11,12 +43,15 @@ int main(int argc , char* argv[]){
         exit(1);
     }
 
-    class_structure* java_class;
-    java_class = ( class_structure* ) malloc( sizeof(class_structure) );
+    class_structure* jclass;
+    FILE *fclass = fopen(argv[1], "rb");
+    jclass = ( class_structure* ) malloc( sizeof(class_structure) );
 
-    printf("Rodou!\n");
+    jclass->magic = read32(fclass);
+    printf("%x\n",jclass->magic);
 
-    free(java_class);
+    free(jclass);
+    fclose(fclass);
 
     return 0;
 }
