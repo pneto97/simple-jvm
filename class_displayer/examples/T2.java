4d82458c30775ac1be7ee53eb064e3273e3eceb6
Paulo Neto <paulo_btn@yahoo.com.br>
2019-05-12 13:14:33 -0300

    adicionando mais exemplos

---
+++ class_displayer/examples/T2.java
@@ -0,0 +1,9 @@
+public class T2{
+
+    public static void main(String[] args) {
+        int a = 2;
+        System.out.println(a);
+    }
+
+}
+
---
+++ class_displayer/examples/T3.java
@@ -0,0 +1,13 @@
+public class T3{
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3};
+        float b = 3.4f;
+        for(int i = 0 ; i < 3 ; i++)
+            System.out.println(a[i]);
+
+        System.out.println(b);
+    }
+
+}
+
