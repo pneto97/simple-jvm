32007b1df90834315f5af0571e62ecb0d3bf099b
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2019-05-16 23:18:57 -0300

    adding a T5.java to test fields_count

---
+++ class_displayer/examples/T5.java
@@ -0,0 +1,10 @@
+public class T5{
+    int flag = 1;
+    int oi = 1;
+    String tchau = "lucas troll";
+    public static void main(String[] args) {
+        int a = 2;
+        System.out.println(a);
+    }
+
+}
