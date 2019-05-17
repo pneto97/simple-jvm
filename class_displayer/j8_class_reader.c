039ec37e3b2509ba7f1f17e7d958853f2e04beab
Christian Luis <christianlmc1@gmail.com>
2019-05-16 23:45:32 -0300

    Adicionando acessores e assinaturas

--- class_displayer/j8_class_reader.c
+++ class_displayer/j8_class_reader.c
@@ -123,6 +123,10 @@ void readInterfaces(FILE *class_file, class_structure* jclass){
     }
 }
 
+void readMethods(FILE *class_file, class_structure* jclass){
+
+}
+
 //desaloca a classe incluindo o utf8 do constant pool
 void freeClass(class_structure *jclass){
     
