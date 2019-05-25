94416c956b4b8207a4108e2f054fa34cb07a728b
Christian Luis <christianlmc1@gmail.com>
2019-05-25 15:07:10 -0300

    Arrumando verificacao de versao

--- class_displayer/main.c
+++ class_displayer/main.c
@@ -1,38 +1,33 @@
 //main.c
+#include "read_utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "read_utils.h"
 
 #ifndef H_J8_CLASS_STRUCTURE
 #define H_J8_CLASS_STRUCTURE
 #include "j8_class_structure.h"
 #endif
 
-#include "j8_class_reader.h"
 #include "j8_class_displayer.h"
+#include "j8_class_reader.h"
 
-int main(int argc , char* argv[]){
-    if(argc != 2){
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         printf("Especifique um arquivo .class!\n");
         exit(1);
     }
 
     FILE *class_file = fopen(argv[1], "rb");
-    if(class_file == NULL){
+    if (class_file == NULL) {
         printf("Erro ao abrir o arquivo!\n");
         exit(1);
     }
 
-    class_structure* jclass;
+    class_structure *jclass;
 
     jclass = readClassFile(class_file);
-
-    if (isClassFile(jclass)) 
-        printClassFile(jclass);
-    else 
-        printf("\n\nErro! O arquivo deve ser um .class!\n\n");
-    
+    printClassFile(jclass);
     freeClass(jclass);
 
     fclose(class_file);
