aa5a2d350d48ee454ee46a6b5e183c31de307a90
Rodrigo <rodrigo.demetrio.palma@gmail.com>
2019-07-09 13:44:51 -0300

    tableswitch

--- examples/T7.java
+++ examples/T7.java
@@ -2,26 +2,15 @@ import java.util.ArrayList;
 
 public class T7
 {
-    private static int car = 4;
+    // private static int car = 4;
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
         int pqpcarai = 4;
-        double lol = 5;
-        ArrayList<String> arraiodopaulin = new ArrayList<String>();
-        int[] jaabre = {1, 2};
-        int[][][] x = new int[6][3][] ;
         int pqp = 11;
         pqp+=5;
 
         float paulin = 99999.99f;
-        if(pqp > 1){
-            System.out.println(13213646);
-            System.out.println(pqpcarai);
-        }
-        else{
-            System.out.println("Goodbye World!");
-        }
 
         switch(pqpcarai) {
         case 2 :
@@ -29,9 +18,6 @@ public class T7
             pqp = 8664;
             break; // optional
         case 3 :
-            // System.out.println(3);
-            // System.out.println(3);
-            // System.out.println(3);
             pqp = 8664;
             break; // optional
         case 4 :
