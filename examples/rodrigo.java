669bb32bbe8e3346ad3c0cff202f8dd503812399
Rodrigo <rodrigo.demetrio.palma@gmail.com>
2019-07-09 22:50:14 -0300

    rodrigo java

--- examples/rodrigo.java
+++ examples/rodrigo.java
@@ -1,12 +1,12 @@
 public class rodrigo{
     public static void main(String[] args) {
-        long[][] myNum = {{10,20,30},{40,50,60}};
+        double[][] myNum = {{10,20,30},{40,50,60}};
         int length = myNum.length;
         if (myNum[1][1] < myNum[0][1]){
-            myNum[0][0] = myNum[1][0] + myNum[1][0];
+            myNum[0][0] = length + myNum[1][0];
         }
         else {
-            myNum[0][0] = myNum[1][2] + myNum[1][0];
+            myNum[0][0] = myNum[1][2] + length;
         }
         
         System.out.println(myNum[0][0]);
